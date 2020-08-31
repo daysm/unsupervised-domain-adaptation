d051a3d986e20c0d532024e5ae39b291c5cd59d8
Dayyan Smith <dayyan.smith@sap.com>
2020-08-31 15:07:07 +0200

    Refactor training.py

--- code/train.py
+++ code/train.py
@@ -132,40 +132,26 @@ def train_source(model, dataloaders, optimizer, args):
             running_loss = 0.0
             running_corrects = 0
 
-            # Iterate over data.
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
-                # zero the parameter gradients
                 optimizer.zero_grad()
 
-                # forward
-                # track history if only in train
                 with torch.set_grad_enabled(phase == "train"):
-                    # Get model outputs and calculate loss
-                    # Special case for inception because in training it has an auxiliary output. In train
-                    #   mode we calculate the loss by summing the final output and the auxiliary output
-                    #   but in testing we only consider the final output.
-                    if is_inception and phase == "train":
-                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
-                        outputs, aux_outputs = model(inputs)
-                        loss1 = criterion(outputs, labels)
-                        loss2 = criterion(aux_outputs, labels)
-                        loss = loss1 + 0.4 * loss2
-                    else:
-                        outputs = model(inputs)
-                        loss = criterion(outputs, labels)
-
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
                     _, preds = torch.max(outputs, 1)
-
-                    # backward + optimize only if in training phase
                     if phase == "train":
                         loss.backward()
                         optimizer.step()
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
+                batch_loss = loss.item()
+                batch_corrects = torch.sum(preds == labels.data)
+                batch_acc = batch_corrects.double() / inputs.size(0)
+                print("{} batch loss: {:.4f} batch acc: {:.4f}".format(phase, loss.item(), batch_acc))
+
+                running_loss += batch_loss * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
@@ -173,7 +159,6 @@ def train_source(model, dataloaders, optimizer, args):
 
             print("{} Loss: {:.4f} Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
 
-            # deep copy the model
             if phase == "val" and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
